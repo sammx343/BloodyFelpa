a87fdba6db9fb0f3ea869510ec6f55814789d694
Jose Paternina <jose.paternina.a@gmail.com>
2020-05-15 19:35:43 -0500

    Level 2 prefabs added

---
+++ Assets/Project/Scripts/Game/Scene/GameCamera2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera2 : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset = new Vector3(3,12,-10);
+
+    private void Update()
+    {
+        Vector3 position = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, position, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Project/Scripts/Game/Weapons/Sword2.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Sword2 : MonoBehaviour
+{
+  // Use this for initialization
+  public float swingingSpeed = 3f;
+  public float attackDuration = .5f;
+  public float cooldownSpeed = 3f;
+  public float cooldownDuration = .5f;
+  public Transform attackPoint;
+  public float attackRange;
+  public LayerMask enemyLayer;
+
+  private float cooldownTimer;
+  private bool isAttacking;
+  private Animator animator;
+  private TrailRenderer trail;
+
+  public bool IsAttacking
+  {
+    get
+    {
+      return isAttacking;
+    }
+  }
+
+
+  void Start(){
+    animator = GetComponentInParent<Animator>();
+    trail = GetComponentInChildren<TrailRenderer>();
+    trail.enabled=false;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+      if(Input.GetKeyDown(KeyCode.E)){
+          Attack();
+      }
+    cooldownTimer -= Time.deltaTime;
+  }
+
+  public void Attack()
+  {
+    animator.SetTrigger("slash");
+    trail.enabled=true;
+    Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position,attackRange, enemyLayer);
+    foreach(Collider enemy in hitEnemies){
+        Debug.Log("Hit"+enemy.name);
+        {
+            
+        }
+    }
+    if (cooldownTimer > 0f)
+    {
+      return;
+    }
+    cooldownTimer = cooldownDuration;
+    StartCoroutine(CooldownWait());
+  }
+
+  private IEnumerator CooldownWait()
+  {
+    isAttacking = true;
+    yield return new WaitForSeconds(attackDuration);
+    trail.enabled=false;
+    isAttacking = false;
+  }
+
+  void OnDrawGizmosSelected(){
+      if(attackPoint==null) 
+        return;
+      Gizmos.DrawWireSphere(attackPoint.position,attackRange);
+  }
+}
